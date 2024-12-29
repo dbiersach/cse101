03faa38fa8e6095e3b5f598984a771bb2cbf0fb0
Dave Biersach <dbiersach@gmail.com>
2024-12-29 15:09:45 -0500

    Initial commit

---
+++ Session 06 - Sampling Theory, Complex Exponentials/plot_superposition.py
@@ -0,0 +1,16 @@
+# plot_superposition.py
+
+import sys
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.linspace(0, 4 * np.pi, 1000)
+r = 7 + 7 * np.sin(11 * t) * np.cos(5 * t)
+
+plt.figure(Path(__file__).name)
+plt.subplot(projection="polar")
+plt.plot(t, r, color="black")
+plt.show(block=True)
+sys.exit()
