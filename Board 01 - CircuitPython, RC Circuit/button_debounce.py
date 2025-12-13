a2788a99727b584bc044279ca4eb3fffdc75c46e
Dave Biersach <dbiersach@gmail.com>
2025-12-13 00:02:33 -0500

    Simplify code

--- Board 01 - CircuitPython, RC Circuit/button_debounce.py
+++ Board 01 - CircuitPython, RC Circuit/button_debounce.py
@@ -13,17 +13,17 @@ from adafruit_debouncer import Debouncer
 # Initialize Adafruit NeoPixel object
 pixel = neopixel.NeoPixel(board.GP22, 1, brightness=0.25)
 
-# Turn off the NeoPixel off (set its color to BLACK)
+# Turn off the NeoPixel off (set its color to BLACK/OFF)
 pixel[0] = (0, 0, 0)
 
 # Initialize GPIO pin for INPUT
 pin_button = digitalio.DigitalInOut(board.GP20)
 pin_button.pull = digitalio.Pull.UP
 
-# Intialize Adafruit Debouncer object
+# Initialize Adafruit Debouncer object
 switch = Debouncer(pin_button, interval=0.05)
 
-# Define the color tuplets for RED, GREEN, and BLUE
+# Define the color tuples for RED, GREEN, BLUE, and BLACK/OFF
 colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 0, 0)]
 
 push_count = 0
@@ -33,5 +33,5 @@ while True:
     if switch.fell:
         # Set the neopixel color using push_count as an index into colors list
         pixel[0] = colors[push_count]
-        # Increment the push_count but remain within interval [0, 2]
+        # Increment the push_count but remain within interval [0, 3]
         push_count = (push_count + 1) % 4
--- Board 01 - CircuitPython, RC Circuit/pwm_led.py
+++ Board 01 - CircuitPython, RC Circuit/pwm_led.py
@@ -10,12 +10,11 @@ import pwmio
 # Initialize GPIO pin for OUTPUT
 pin_led = pwmio.PWMOut(board.GP22, frequency=5000, duty_cycle=0)
 
-
 while True:
     # Fade from off to on
-    for duty in range(0, 65536, 85):  # 0 to 65535
+    for duty in range(0, 65536, 256):  # 0 to 65535 in steps of 256
         pin_led.duty_cycle = duty
 
     # Fade from on to off
-    for duty in range(65535, -1, -85):
+    for duty in range(65535, -1, -256):
         pin_led.duty_cycle = duty
